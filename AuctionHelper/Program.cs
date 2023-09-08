e20bf9dffa7295f698ee728afb7293d9d77a5777
MrSlimeDude <45675291+MrSlimeDude@users.noreply.github.com>
2023-09-08 17:26:39 -0400

    Give the bot status

--- AuctionHelper/Program.cs
+++ AuctionHelper/Program.cs
@@ -12,6 +12,10 @@ namespace AuctionHelper
     { 
         private static List<Website> websites = new List<Website>();
 
+        public static int BuildNum;
+        public static string CommitHash;
+        public static string CommitHashShort;
+
         public static void Main(string[] args)
         {
             websites.Add(new RideauActions());
@@ -28,16 +32,67 @@ namespace AuctionHelper
             //await fetcher.GetAuctionItemsAsync();
             //return;
 
+            // Parse build and hash
+            if(File.Exists("ver.info"))
+            {
+                string[] verLines = File.ReadAllLines("ver.info");
+                foreach (string s in verLines)
+                {
+                    string[] split = s.Split('=');
+
+                    if (split.Length != 2)
+                        continue;
+
+                    string key = split[0];
+                    string value = split[1];
+
+                    if (key == "BUILD_NUM")
+                    {
+                        BuildNum = int.Parse(value);
+                    }
+                    else if (key == "COMMIT")
+                    {
+                        CommitHash = value;
+                        CommitHashShort = value[..7];
+                    }
+                }
+            }
+            else
+            {
+                BuildNum = -1;
+                CommitHash = "";
+                CommitHashShort = "";
+            }
+
             var discord = new DiscordClient(new DiscordConfiguration()
             {
-                //Token = "MTA3MDIxMDUzODc0OTA1NTAzNg.GCWbLv.9BKGzd-yp6UsflGm_DMWxa_V9aeW-c4_EeDTUc", // DEV
-                //Token = "OTg2MDgzMTU2OTM3NTY0MjMw.Gye9rZ.3Dv2xDJes2RHGmZXbMy6GJhIi3GqZEJCE6Fn7c", // REAL
                 Token = Environment.GetEnvironmentVariable("TOKEN") ?? "INVALID_TOKEN",
                 TokenType = TokenType.Bot,
-                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents | DiscordIntents.GuildPresences
             });
 
             HtmlWeb web = new HtmlWeb();
+
+            discord.Ready += async (s, e) =>
+            {
+                DiscordActivity activity = new DiscordActivity();
+                activity.ActivityType = ActivityType.Competing;
+                activity.Name = "the top auctions";
+                await discord.UpdateStatusAsync(activity, UserStatus.Online);
+
+                foreach (var guild in s.Guilds.Values)
+                {
+                    var member = await guild.GetMemberAsync(discord.CurrentUser.Id);
+                    if (member.Nickname != null && (!Regex.IsMatch(member.Nickname, "Auction Helper \\(#-?\\d+\\)") && member.Nickname != "Auction Helper"))
+                        continue; // Only continue if we have an update
+
+                    await member.ModifyAsync(a =>
+                    {
+                        a.Nickname = $"Auction Helper (#{BuildNum})";
+                        a.AuditLogReason = "Bot updated";
+                    });
+                }
+            };
 
             discord.MessageCreated += async (s, e) =>
             {
@@ -50,7 +105,8 @@ namespace AuctionHelper
                 }
             };
 
-            await discord.ConnectAsync();
+            await discord.ConnectAsync();  
+
             await Task.Delay(-1);
         }
     }
